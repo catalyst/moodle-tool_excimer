fda3da440891fe8a756d6e9e27513766215d2be2
Mike Macgirvin <mikemacgirvin@catalyst-au.net>
2022-03-31 13:27:02 +1100

    Issue #181: add support for $markup (separate presentation for viewing vs. downloading)

--- classes/helper.php
+++ classes/helper.php
@@ -92,21 +92,25 @@ class helper {
      * @throws \Exception
      */
     public static function duration_display(float $duration, bool $markup = true): string {
-        // Variable $markup allows a different format when downloaded vs viewed. It is currently unused.
-        if (intval($duration) > 10) {
-            // Use whole seconds.
-            $usetime = intval($duration);
-        } else {
-            // Add one decimal place.
-            $usetime = round($duration, 1);
-            // Fallback case to prevent format_time() returning the translated string 'now' when $usetime is less than 100ms.
-            // It will still appear if less than 1ms rounded. Discuss.
-            if ($usetime < 0.1) {
-                $usetime = round($duration, 3);
+        // Variable $markup allows a different format when viewed (true) vs downloaded (false).
+        if ($markup) {
+            if (intval($duration) > 10) {
+                // Use whole seconds.
+                $usetime = intval($duration);
+            } else {
+                // Add one decimal place.
+                $usetime = round($duration, 1);
+                // Fallback case to prevent format_time() returning the translated string 'now' when $usetime is less than 100ms.
+                // It will still appear if less than 1ms rounded. Discuss.
+                if ($usetime < 0.1) {
+                    $usetime = round($duration, 3);
+                }
             }
+            // This currently works with floats, but relies on undocumented behaviour of format_time(), which normally takes an int.
+            return format_time($usetime);
         }
-        // This currently works with floats, but relies on undocumented behaviour of format_time(), which normally takes an int.
-        return format_time($usetime);
+        // When downloading just provide the float.
+        return $duration;
     }
 
     /**
