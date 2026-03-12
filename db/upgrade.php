124a6cb78c3014cd50cc166b7ef841d3d1ff04da
Nathan Nguyen <nathannguyen@catalyst-au.net>
2026-03-12 17:03:29 +1100

    Fix error with upgrade script

--- db/upgrade.php
+++ db/upgrade.php
@@ -578,7 +578,7 @@ function xmldb_tool_excimer_upgrade($oldversion) {
     if ($oldversion < 2026031200) {
         // Define field numlogentries to be added to tool_excimer_profiles.
         $table = new xmldb_table('tool_excimer_profiles');
-        $field = new xmldb_field('numlogentries', XMLDB_TYPE_INTEGER, '11', null, XMLDB_NOTNULL, null, null, 'numsamples');
+        $field = new xmldb_field('numlogentries', XMLDB_TYPE_INTEGER, '11', null, XMLDB_NOTNULL, null, 0, 'numsamples');
 
         // Conditionally launch add field numlogentries.
         if (!$dbman->field_exists($table, $field)) {
