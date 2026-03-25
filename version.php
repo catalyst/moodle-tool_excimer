dffa7008a7f876a2e74747e1816ccb006be125cb
Brendan Heywood <brendan@catalyst-au.net>
2026-03-25 11:23:17 +1100

    Bump version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026031701;
-$plugin->release = 2026031701;
+$plugin->version = 2026031702;
+$plugin->release = 2026031702;
 $plugin->requires  = 2023100900; // Moodle 4.3.
 $plugin->supported = [403, 405];
 $plugin->component = 'tool_excimer';
